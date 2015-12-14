0c3aa5978c47267df97ef4977b00ad84c3928fe2
baisusu <yuan404kang@126.com>
2015-12-14 17:33:28 +0800

    增加node 服务和转移html文件

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 var gulp          = require('gulp');
 var webpack       = require('gulp-webpack');
 
-gulp.task("msc_index", function() {
+gulp.task("index_html", function() {
     return gulp
-        .src('./app/lib/page/*.js')
+        .src('./app/index.html')
+        .pipe(gulp.dest('./dev/'));
+});
+
+gulp.task("index_js", function() {
+    return gulp
+        .src('./app/lib/page/index.js')
         .pipe(webpack({
         	watch: true,
         	output: {
-		        filename: 'index.js'
+		        filename: 'index.min.js'
 		    },
         	module:{
 	            loaders: [{
@@ -20,5 +26,7 @@ gulp.task("msc_index", function() {
 	        },
 	        devtool: 'source-map'
         }))
-        .pipe(gulp.dest('./dev/'));
+        .pipe(gulp.dest('./dev/js/'));
 });
+
+gulp.task('msc_index',['index_html','index_js']);
--- server.js
+++ server.js
@@ -1,68 +1,43 @@
-/**
- * This file provided by Facebook is for non-commercial testing and evaluation
- * purposes only. Facebook reserves all rights not expressly granted.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
+var PORT = 3000;
 
-var fs = require('fs');
-var path = require('path');
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
+var http = require('http');
+var url=require('url');
+var fs=require('fs');
+var mine=require('./mine').types;
+var path=require('path');
 
-var COMMENTS_FILE = path.join(__dirname, 'comments.json');
+var server = http.createServer(function (request, response) {
+    var pathname = url.parse(request.url).pathname;
+    var realPath = path.join("assets", pathname);
+    //console.log(realPath);
+    var ext = path.extname(realPath);
+    ext = ext ? ext.slice(1) : 'unknown';
+    fs.exists(realPath, function (exists) {
+        if (!exists) {
+            response.writeHead(404, {
+                'Content-Type': 'text/plain'
+            });
 
-app.set('port', (process.env.PORT || 3000));
-
-app.use('/', express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.get('/api/comments', function(req, res) {
-  fs.readFile(COMMENTS_FILE, function(err, data) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    res.setHeader('Cache-Control', 'no-cache');
-    res.json(JSON.parse(data));
-  });
-});
-
-app.post('/api/comments', function(req, res) {
-  fs.readFile(COMMENTS_FILE, function(err, data) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    var comments = JSON.parse(data);
-    // NOTE: In a real implementation, we would likely rely on a database or
-    // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
-    // treat Date.now() as unique-enough for our purposes.
-    var newComment = {
-      id: Date.now(),
-      author: req.body.author,
-      text: req.body.text,
-    };
-    comments.push(newComment);
-    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-      res.setHeader('Cache-Control', 'no-cache');
-      res.json(comments);
+            response.write("This request URL " + pathname + " was not found on this server.");
+            response.end();
+        } else {
+            fs.readFile(realPath, "binary", function (err, file) {
+                if (err) {
+                    response.writeHead(500, {
+                        'Content-Type': 'text/plain'
+                    });
+                    response.end(err);
+                } else {
+                    var contentType = mine[ext] || "text/plain";
+                    response.writeHead(200, {
+                        'Content-Type': contentType
+                    });
+                    response.write(file, "binary");
+                    response.end();
+                }
+            });
+        }
     });
-  });
 });
-
-
-app.listen(app.get('port'), function() {
-  console.log('Server started: http://localhost:' + app.get('port') + '/');
-});
+server.listen(PORT);
+console.log("Server runing at port: " + PORT + ".");
